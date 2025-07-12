4dc32d845c70016a160394ad8edd38fd5be3b4d7
LindaAlsalhi <lindaalsalhi2001@gmail.com>
2025-07-12 15:46:40 +0300

    first commit

---
+++ TrainingPlatform.Api/TrainingPlatform.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingPlatform.Core.Entities;
+
+namespace TrainingPlatform.Infrastructure.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext( DbContextOptions<AppDbContext> options):base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Course> Courses => Set<Course>();
+
+    }
+}
